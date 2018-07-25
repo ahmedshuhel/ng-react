13fdb1f92aba18c11323859ecc60255eb869a682
Shuhel Ahmed <ahmedshuhel@newscred.com>
2018-07-25 15:15:08 +0600

    Add debounce to autocomplete

--- src/react-components/autocomplete.js
+++ src/react-components/autocomplete.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import axios from 'axios';
+import debounce from 'lodash.debounce';
 
 export class Autocomplete extends React.Component {
   constructor(props) {
     super(props);
-
     this.state = {
       countries: []
     };
+    this.search = debounce(this._search, 500);
   }
 
-  onChange = (ev) => {
-    let q = ev.target.value;
-    if (q.length < 2) return;
+  onChange = (event) => {
+    let q = event.target.value;
+    this.search(q);
+  }
 
+  _search(q) {
+    if (q.length < 2) {
+      this.setState({
+        countries: res.data
+      });
+      return;
+    }
     axios.get(`https://restcountries.eu/rest/v2/name/${q}`)
       .then(res => {
         this.setState({
